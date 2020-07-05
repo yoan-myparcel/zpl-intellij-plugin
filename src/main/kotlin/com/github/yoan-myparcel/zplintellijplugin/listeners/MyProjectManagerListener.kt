50aa3c73d152e28c1b446f427b2c5fc2cd81353a
GitHub Action <action@github.com>
2020-07-05 09:32:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/yoan-myparcel/zplintellijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/yoan-myparcel/zplintellijplugin/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.yoan-myparcel.zplintellijplugin.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yoan-myparcel.zplintellijplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/yoan-myparcel/zplintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.yoan-myparcel.zplintellijplugin.services
+
+import com.github.yoan-myparcel.zplintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
